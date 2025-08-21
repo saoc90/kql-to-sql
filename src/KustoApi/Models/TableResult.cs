d296f5970797bf2df75c287f78aec5f9d0695e4b
Samuel Ochsner <samuel.ochsner@buhlergroup.com>
2025-08-21 11:44:51 +0200

    Enhance Kusto API with self-signed certificate handling, CORS support, and improved query response structure

---
+++ src/KustoApi/Models/TableResult.cs
@@ -0,0 +1,13 @@
+﻿namespace KustoApi.Models;
+
+public class TableResult
+{
+    public IEnumerable<Table> Table { get; set; } = [];
+}
+
+public class Table
+{
+    public string TableName { get; set; } = string.Empty;
+    public List<Column> Columns { get; set; } = new List<Column>();
+    public List<List<object>> Rows { get; set; } = new List<List<object>>();
+}
